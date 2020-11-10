9ee76ed13c74001f50dcf4fbf42954a17c87369a
ankush <bhanusinghank@gmail.com>
2020-11-10 21:18:54 +0530

    post and login function completed

--- fittr.py
+++ fittr.py
@@ -3,76 +3,90 @@ from selenium.webdriver import ActionChains
 import pyautogui
 import time
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 
 driver.get("https://www.fittr.com/dashboard/home/social/all")
 
-email = input("Enter email to sign in : ")
+def login():
+    emailid = input("Enter email : ")
+    passw = input("Enter password : ")
+    #emailid = ""
+    #passw = ""
 
-#password = input("Enter password")
-#profile = input("Enter profile link")
+    email = driver.find_element_by_xpath('//*[@id="adornment-username"]')
+    email.send_keys(emailid)
+    time.sleep(4)
+    password = driver.find_element_by_xpath('/html/body/div[4]/div[4]/div[1]/div[1]/div/div[2]/div/form/div[2]/div/div/div/input')
+    password.send_keys(passw)
+    time.sleep(1)
+    pyautogui.click(462, 635)
+    time.sleep(10)
 
-# add post
-add_post = driver.find_element_by_css_selector(".addpost_btn")
-add_post.click()
-time.sleep(2)
+def post():
+    # add post
+    add_post = driver.find_element_by_css_selector(".addpost_btn")
+    add_post.click()
+    time.sleep(2)
 
-# recipie button
-recipe = driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[2]/main/div[1]/div[1]/div/nav/div[3]/div/div/div/div[2]/span[3]')
-recipe.click()
-time.sleep(1)
-#pyautogui.click(472, 290)
+    # recipie button
+    recipe = driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[2]/main/div[1]/div[1]/div/nav/div[3]/div/div/div/div[2]/span[3]')
+    recipe.click()
+    time.sleep(1)
+    #pyautogui.click(472, 290)
 
-# image upload
-imageupload = driver.find_element_by_xpath("//input[@type='file']")
-imageupload.send_keys("/home/ankush/Downloads/download.jpeg")
+    # image upload
+    imageupload = driver.find_element_by_xpath("//input[@type='file']")
+    imageupload.send_keys("/home/ankush/Downloads/download.jpeg")
 
-# recipe name
-recipename = driver.find_elements_by_id("outlined-dense")[0]
-recipename.send_keys("Egg Salad")
+    # recipe name
+    recipename = driver.find_elements_by_id("outlined-dense")[0]
+    recipename.send_keys("Egg Salad")
 
-# recipe type
-recipetype = driver.find_elements_by_id("mui-component-select-recipe_type")[0]
-recipetype.click()
-time.sleep(1)
-pyautogui.click(598, 788)
-time.sleep(1)
+    # recipe type
+    recipetype = driver.find_elements_by_id("mui-component-select-recipe_type")[0]
+    recipetype.click()
+    time.sleep(1)
+    pyautogui.click(1072, 758)
+    time.sleep(1)
 
-# region
-#region = driver.find_elements_by_xpath('/html/body/div[3]/div[3]/div[2]/main/div[1]/div[1]/div/nav/div[3]/div/div/div/div[3]/div/form/div[4]/div[1]/div/div/div/div')[0]
-#region.click()
-pyautogui.click(293, 850)
-time.sleep(1)
-pyautogui.click(284, 811)
+    # region
+    #region = driver.find_elements_by_xpath('/html/body/div[3]/div[3]/div[2]/main/div[1]/div[1]/div/nav/div[3]/div/div/div/div[3]/div/form/div[4]/div[1]/div/div/div/div')[0]
+    #region.click()
+    pyautogui.click(777, 855)
+    time.sleep(1)
+    pyautogui.click(777,855)
 
-# time
-timet = driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[2]/main/div[1]/div[1]/div/nav/div[3]/div/div/div/div[3]/div/form/div[4]/div[2]/div/div/div/div/div/input')
-time.sleep(1)
-timet.send_keys("0012")
+    # time
+    timet = driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[2]/main/div[1]/div[1]/div/nav/div[3]/div/div/div/div[3]/div/form/div[4]/div[2]/div/div/div/div/div/input')
+    time.sleep(1)
+    timet.send_keys("0012")
 
-# protien
-protien = driver.find_elements_by_id("outlined-dense")[1]
-protien.send_keys("13")
+    # protien
+    protien = driver.find_elements_by_id("outlined-dense")[1]
+    protien.send_keys("13")
 
-# carbs
-carbs = driver.find_elements_by_id("outlined-dense")[2]
-carbs.send_keys("10")
+    # carbs
+    carbs = driver.find_elements_by_id("outlined-dense")[2]
+    carbs.send_keys("10")
 
-# fats
-fats = driver.find_elements_by_id("outlined-dense")[3]
-fats.send_keys("7")
+    # fats
+    fats = driver.find_elements_by_id("outlined-dense")[3]
+    fats.send_keys("7")
 
-# ingredients
-ingredients = driver.find_elements_by_id("outlined-dense")[4]
-ingredients.send_keys("eggs, dill, chives, celery and onion in creamy garlic mayo")
+    # ingredients
+    ingredients = driver.find_elements_by_id("outlined-dense")[4]
+    ingredients.send_keys("eggs, dill, chives, celery and onion in creamy garlic mayo")
 
-# directions
-directions = driver.find_elements_by_id("outlined-dense")[5]
-directions.send_keys("1) Hard boil eggs on the stovetop or in the instant pot and cool. Once cooled, cut into desired pieces (we like ours chunky) 2) Finely cube the celery and red onion (sweet onion may be substituted for red onion)")
+    # directions
+    directions = driver.find_elements_by_id("outlined-dense")[5]
+    directions.send_keys("Hard boil eggs on the stovetop or in the instant ")
 
-# post
-post = driver.find_element_by_xpath('//*[@id="post-wrapper-sidebar"]/div/div/div[3]/div/form/div[7]/button')
-time.sleep(4)
-post.click()
+    # post
+    post = driver.find_element_by_xpath('/html/body/div[3]/div[3]/div[2]/main/div[1]/div[1]/div/nav/div[3]/div/div/div/div[3]/div/form/div[7]/button/span')
+    time.sleep(4)
+    post.click()
 
+#def delete():
 
+login()
+post()
